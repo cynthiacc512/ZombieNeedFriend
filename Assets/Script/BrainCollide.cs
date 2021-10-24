ae11a36734eca92e7f99af8f85b7d3489db9e0c8
Fera <christy.ferani@gmail.com>
2021-10-25 02:37:41 +0700

    Perbaikan Bug and UI

--- Assets/Script/BrainCollide.cs
+++ Assets/Script/BrainCollide.cs
@@ -6,33 +6,17 @@ public class BrainCollide : MonoBehaviour
 {
     public GameObject theZombie;
 	public GameObject zombieRage;
-    float x, z;
-
 	
 	// Update is called once per frame
     void Update()
     {
-        // Rage selama 10 detik
-        // float timeRemaining = 10f;
 
         if(PlayerCollision.isRage && !zombieRage.activeSelf){
 			zombieRage.SetActive(true);
             zombieRage.transform.position = theZombie.transform.position;
             theZombie.SetActive(false);
-
-            // Masih error
-            // while (timeRemaining > 0){
-            //     timeRemaining -= Time.deltaTime;
-            //     Debug.Log("timeRemaining: " + timeRemaining);
-            // }
-
-            // if (timeRemaining <= 0){
-            //     Debug.Log("ganti zombie");
-            //     theZombie.SetActive(true);
-            //     theZombie.transform.position = zombieRage.transform.position;
-            //     zombieRage.SetActive(false);
-            // }
             
+            RageTimer.timeRemaining = 5;
         }
     }
 }
--- Assets/Script/RotateItems.cs
+++ Assets/Script/RotateItems.cs
@@ -1,35 +1,35 @@
-﻿// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-// public class RotateItems : MonoBehaviour
-// {
-// 	public GameObject theBrain;
+public class RotateItems : MonoBehaviour
+{
+	public GameObject theBrain;
 	
-// 	public float PlaceX;
-// 	public float PlaceZ;
-// 	public int itemCount = 0;
+	public float PlaceX;
+	public float PlaceZ;
+	public int itemCount = 0;
 	
-// 	void Start()
-//     {
-//         StartCoroutine(ItemDrop());
-//     }
+	void Start()
+    {
+        StartCoroutine(ItemDrop());
+    }
 	
-// 	// Update is called once per frame
-//     void Update()
-//     {
-//         theBrain.transform.Rotate(new Vector3(0f, 100f, 0f) * Time.deltaTime);
-//     }
+	// Update is called once per frame
+    void Update()
+    {
+        theBrain.transform.Rotate(new Vector3(0f, 100f, 0f) * Time.deltaTime);
+    }
 	
-//     IEnumerator ItemDrop()
-// 	{
-// 		while(itemCount < 4){
-// 			PlaceX = Random.Range(2.2f, 10);
-// 			PlaceZ = Random.Range (48, 783);
-// 			Instantiate(theBrain, new Vector3(PlaceX, 1, PlaceZ), Quaternion.Euler(0f, 0f, 0f));
-// 			yield return new WaitForSeconds(4f);
-// 			itemCount += 1;
-// 		}
-// 	}
+    IEnumerator ItemDrop()
+	{
+		while(itemCount < 4){
+			PlaceX = Random.Range(2.2f, 10);
+			PlaceZ = Random.Range (48, 783);
+			Instantiate(theBrain, new Vector3(PlaceX, 1, PlaceZ), Quaternion.Euler(0f, 0f, 0f));
+			yield return new WaitForSeconds(4f);
+			itemCount += 1;
+		}
+	}
 
-// }
+}
